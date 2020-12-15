2db7a1e94d74300b706208b3319ff59a2d67fafe
Daniel <rodriguez.flores.daniel01@gmail.com>
2020-12-15 02:55:01 +0000

    Se modifico la ruta del socket

--- public/js/logica.js
+++ public/js/logica.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8080');
+var socket = io.connect('process.env.PORT');
 var list = document.querySelector('#lista-users');
 var username = window.location.pathname.replace('/chat/', '');
 var clientes = [];
